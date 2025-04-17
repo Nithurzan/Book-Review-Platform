96bbd61979352d56c75fa3ce3e00299490a1cfeb
Y.Nithurzan <119398278+Nithurzan@users.noreply.github.com>
2025-04-17 19:03:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,5 +25,5 @@ app.use("/api/users", userRoutes);
 // Error handler middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || https://book-review-platform-vsyh.vercel.app/;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
